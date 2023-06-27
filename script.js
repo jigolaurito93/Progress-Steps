ab014ba33a5dbf4c6998fb2647926efd5c169f60
Jose Laurito <jigolaurito93@yahoo.com>
2023-06-27 00:43:31 -0500

    added eventlistener to PREV and NEXT buttons

--- script.js
+++ script.js
@@ -2,3 +2,45 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElement('next');
 const circles = document.querySelectorAll('.circle');
+
+// Whichever one is active is gonna set 1 by default
+let currentActive = 1
+
+// Add event listener
+
+next.addEventListener('click', () => {
+    // Increment current active by 1 
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+    update()
+})
+
+prev.addEventListener('click', () => {
+    // Decrement current active by 1
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+
+    update()
+})
+
+
+// Create a function called update
+function update() {
+    // take circles and loop through with foreach
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+}
